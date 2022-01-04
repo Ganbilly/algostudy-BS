c59cd8c86954685b6122b35fbe97a2520df12c26
chs5306 <scott9503@nate.com>
2022-01-04 21:43:40 +0900

    2022.01.04.hs

---
+++ C++basic/[5]1차원 배열/[10818]최소,최대/hs10818.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int min, max;
+    int num;
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+
+        if(min >= num)
+            min = num;
+         
+        if(max <= num)
+            max = num;
+    }
+    cout << min << " " << max;
+
+    return 0;
+}
