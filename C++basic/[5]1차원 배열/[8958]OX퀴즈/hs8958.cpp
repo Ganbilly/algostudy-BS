7b0058e27fa5c016b367b5747a2e9037e577b8b7
chs5306 <scott9503@nate.com>
2022-01-08 23:30:41 +0900

    2022.01.08.hs

--- C++basic/[5]1차원 배열/[8958]OX퀴즈/hs8958.cpp
+++ C++basic/[5]1차원 배열/[8958]OX퀴즈/hs8958.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main()
@@ -6,7 +7,7 @@ int main()
     int n;
     int count;
     int num;
-    char arr[80] = {0};
+    char str[80] = {0};
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,23 +19,24 @@ int main()
     {
         num = 0;
         count = 0;
-        cin >> arr;
-    }
+        cin >> str;
 
-    for (int i = 0; arr[i] != 0; i++)
-    {
-        if (arr[i] == 0)
+        for (int j = 0; str[j] != 0; j++)
         {
-            count++;
-            num += count;
-        }
+            if (str[j] == 'O')
+            {
+                count++;
+                num += count;
+            }
 
-        else
-        {
-            count = 0;
+            else
+            {
+                count = 0;
+            }
         }
+        cout << num << '\n';
+        memset(str, 0, sizeof(str));
     }
-    cout << num << '\n';
 
     return 0;
 }
---
+++ C++basic/[6]함수/[4673]셀프 넘버/hs4673.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int d(int n)
+{
+    int sum;
+}
