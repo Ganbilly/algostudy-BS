03d08874c48d6a2eb0982b332d8ac2c4a28c4598
chs5306 <scott9503@nate.com>
2022-01-09 21:32:00 +0900

    2022.01.09.hs

---
+++ C++basic/2022.01.09.모임/hs2231.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int result;
+int sum;
+int arr[1000000] = {0};
+
+int main()
+{
+    for (int i = 1; i < 1000000; i++)
+    {
+        result = i;
+        sum = i;
+
+        while (sum > 0)
+        {
+            result = result + (sum % 10);
+            sum = sum / 10;
+        }
+
+    }
+}
