82e7ee38c4a41068384f0b4f664b1b3390ec5314
Ganbilly <les1533@gmail.com>
2022-01-17 23:48:42 +0900

    2022.01.17.es

--- C++basic/2022.01.14.모임/[1018]/es1018.cpp
+++ C++basic/2022.01.14.모임/[1018]/es1018.cpp
@@ -6,24 +6,47 @@ using namespace std;
 int main(){
 
     int n, m;                                //n의 세로의 수, m은 가로의 수
-    int result;
+    int bc, wc, result = 64;
     vector<int> v;                           //경우의 수 저장벡터
     string *board;                           //입력받은 보드    
+    string b[8];
+    string w[8];
 
     cin >> n >> m; 
 
     board = new string[n];                   // 동적배열 할당 (m * n)                    
     
-    for(int i = 0; i < n; i ++)              // 보드에 값 저장
+    for(int i = 0; i < n; i ++)              // 보드에 입력받은 값 저장
         cin >> board[i];
     
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            
+    for(int i = 0; i < 8; i++){
+        if(i % 2 == 0){
+            b[i] = "BWBWBWBW";
+            w[i] = "WBWBWBWB";
+            continue;
         }
+        b[i] = "WBWBWBWB";
+        w[i] = "BWBWBWBW";
     }
 
 
-    sort(v.begin(), v.end());
+    for(int i = 0; i <= n - 8; i++){
+        for(int j = 0; j <= m - 8; j++){
+            bc = 0;
+            wc = 0;
+            for(int k = 0; k < 8; k++){
+                for(int l = 0; l < 8; l++){
+                    if(board[i + k][j + l] != b[k][l])
+                        bc++;
+                    else
+                        wc++;
+                }
+            }
+            if( result > min(bc, wc))
+                result = min(bc, wc);
+        }
+    }
+
+    cout << result;
     return 0;
 }
