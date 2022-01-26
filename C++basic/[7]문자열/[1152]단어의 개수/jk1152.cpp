e476fe9dee431f0e01ec6a8f4fad7b7fa2ee9e7a
OheWaeJook <worl5@korea.ac.kr>
2022-01-27 01:12:33 +0900

    2022.01.27.jk

--- C++basic/[7]문자열/[1152]단어의 개수/jk1152.cpp
+++ C++basic/[7]문자열/[1152]단어의 개수/jk1152.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 int main()
 {
+    // 스페이스바를 카운트하면 안됨 (문자열 시작과 끝에 스페이스바가 있으면 곤란)
+    // ->스페이스바가 아닌 문자가 오면 카운팅되게
     string ins;
     int count = 0;
 
-    cin >> ins;
+    getline(cin, ins); //공백 포함한 문자열 받기
+    // cin >> ins;
     for (int i = 0; i < ins.length(); i++)
     {
-        if (ins[i] == ' ')
+        if (ins[i] != ' ')
         {
             count++;
+            if (ins[i] == ' ')
+            {
+                        }
         }
     }
     cout << count;
