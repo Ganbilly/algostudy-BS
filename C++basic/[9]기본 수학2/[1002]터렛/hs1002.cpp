3f242ea654cc51c280d925a2a17ea15fc00134f5
chs5306 <scott9503@nate.com>
2022-02-05 22:03:08 +0900

    2022.02.05.hs

--- C++basic/[9]기본 수학2/[1002]터렛/hs1002.cpp
+++ C++basic/[9]기본 수학2/[1002]터렛/hs1002.cpp
@@ -12,7 +12,23 @@ int main()
         cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
         int d = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
-    }
+        int nejub = (r1 - r2) * (r1 - r2);
+        int owejub = (r1 + r2) * (r1 + r2);
 
+        if (d == 0) 
+        {
+            if (nejub == 0)
+                cout << "-1" << '\n';
+            else
+                cout << "0" << '\n';
+        }
+
+        else if (d == nejub || d == owejub)
+            cout << "1" << '\n';
+        else if (nejub < d && d < owejub)
+            cout << "2" << '\n';
+        else
+            cout << "0" << '\n';
+    }
     return 0;
 }
