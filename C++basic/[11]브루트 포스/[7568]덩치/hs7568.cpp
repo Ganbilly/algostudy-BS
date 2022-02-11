e815286ec2d254722671cc3bdf6c204e89bcb666
chs5306 <scott9503@nate.com>
2022-02-11 21:49:33 +0900

    2022.02.11.hs

--- C++basic/[11]브루트 포스/[7568]덩치/hs7568.cpp
+++ C++basic/[11]브루트 포스/[7568]덩치/hs7568.cpp
@@ -6,12 +6,28 @@ int main()
     int n;
     cin >> n;
 
-    int w;
-    int h;
+    int w[50] = {0};
+    int h[50] = {0};
+    int rank[50];
 
     for (int i = 0; i < n; i++)
     {
-        
+        cin >> w[i] >> h[i];
+        rank[i] = 1;
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (w[i] < w[j] && h[i] < h[j])
+                rank[i]++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << rank[i] << " ";
+    }
+    return 0;
 }
